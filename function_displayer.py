84c08c3dd2c8e0f71e27ed79c7ae20fe6807598a
Arseny Neustroev <neustroev.arseny@gmail.com>
2018-01-23 19:49:58 +0500

    some shit happened

--- function_displayer.py
+++ function_displayer.py
@@ -2,8 +2,7 @@ from tkinter import *
 
 
 class Displayer(Canvas):
-    def __init__(self, root, scale_x=10, scale_y=1, canvas_size=500):
-        self.inc = canvas_size // 2
+    def __init__(self, root, scale_x=10, scale_y=1):
         super(Displayer, self).__init__(root, width=500, height=500, bg="white")
         self.create_line(250, 500, 250, 0, width=1, arrow=LAST)  # drawing y_axis
         self.create_line(0, 250, 500, 250, width=1, arrow=LAST)  # drawing x_axis
