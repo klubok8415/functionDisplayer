1044f01bcfec20030f615728a7a60ea222865090
Arseny Neustroev <neustroev.arseny@gmail.com>
2018-01-24 13:48:43 +0500

    rescaling (try)

--- function_displayer.py
+++ function_displayer.py
@@ -2,46 +2,48 @@ from tkinter import *
 
 
 class Displayer(Canvas):
-    def __init__(self, root, x_min=-5, y_min=-25, x_max=5, y_max=25, x_scale=1, y_scale=1):
+    def __init__(self, root, x_scale=1, y_scale=1):
         super(Displayer, self).__init__(root, width=510, height=510, bg="white")
-        self.create_line(250, 500, 250, 0, width=1, arrow=LAST)  # drawing y_axis
-        self.create_line(0, 250, 500, 250, width=1, arrow=LAST)  # drawing x_axis
-        self.x_max = x_max
-        self.x_min = x_min
-        self.y_min = y_min
-        self.y_max = y_max
+        self.x_scale = x_scale
+        self.y_scale = y_scale
+
+    def add_axis(self, x_min=-5, x_max=5, y_min=-25, y_max=25):
+        x_min = int(x_min)
+        x_max = int(x_max)
+        y_min = int(y_min)
+        y_max = int(y_max)
+        self.create_line(250, 500, 250, 0, width=1, arrow=LAST)
+        self.create_line(0, 250, 500, 250, width=1, arrow=LAST)
+
         # marking x_axis
         for i in range(501):
             if i % 50 == 0:
                 k = -250 + i
-                self.create_line(k+250, -2+250, k+250, 2+250,
-                                         width=0.25, fill='black')
+                self.create_line(k + 250, -2 + 250, k + 250, 2 + 250,
+                                 width=0.25, fill='black')
 
                 self.create_text(k + 250, -10 + 250,
-                                 text=str(k * y_scale * (self.x_max - self.x_min) // (x_scale * 500)), fill='black',
+                                 text=str(k * self.y_scale * (x_max - x_min) // (self.x_scale * 500)), fill='black',
                                  font=('Helvectica', '10'))
         # marking y_axis
         for j in range(501):
             if j % 50 == 0:
                 k = -250 + j
                 if k != 0:
-                    self.create_line(-2+250, k+250, 2+250, k+250,
-                                             width=0.25, fill='black')
+                    self.create_line(-2 + 250, k + 250, 2 + 250, k + 250,
+                                     width=0.25, fill='black')
 
-                    self.create_text(10+250, k+250,
-                                             text=str(k * (self.y_max - self.y_min) // 500), fill='black',
-                                             font=('Helvectica', '10'))
+                    self.create_text(10 + 250, k + 250,
+                                     text=str(k * (y_max - y_min) // 500), fill='black',
+                                     font=('Helvectica', '10'))
 
-        self.scale_x = x_scale
-        self.scale_y = y_scale
-
-    def add_function(self, f, color="black"):
+    def add_function(self, f, x_min=-5, x_max=5, y_min=-25, y_max=25, color="black"):
         previous_point = [0, 0]
         for x in range(-251, 250):
-            x = x * (self.x_max - self.x_min) / 500
+            x = x * (x_max - x_min) / 500
             try:
-                point = [x * 500 // (self.x_max - self.x_min) + 250,
-                         250 - (f(x / self.scale_x) * self.scale_y) * 500 // (self.y_max - self.y_min)]
+                point = [x * 500 // (x_max - x_min) + 250,
+                         250 - (f(x / self.x_scale) * self.y_scale) * 500 // (y_max - y_min)]
                 self.create_line(previous_point, point, fill=color)
                 previous_point = point
             except:
@@ -49,28 +51,28 @@ class Displayer(Canvas):
 
     def add_point(self, x, y, color="black"):
         self.create_oval(
-            self.scale_x * x - 1,
-            500 - self.scale_y * y + 1,
-            self.scale_x * x + 1,
-            500 - self.scale_y * y - 1,
+            self.x_scale * x - 1,
+            500 - self.y_scale * y + 1,
+            self.x_scale * x + 1,
+            500 - self.y_scale * y - 1,
             fill=color)
-
-    @staticmethod
-    def rescale():
-        x_min = Handler.x_min_entry.get()
-        print(x_min)
-
 
 
 class Handler(Frame):
-    def __init__(self, root):
+    def __init__(self, root, displayer):
         super(Handler, self).__init__(root)
+        self.displayer = displayer
 
         # Entries and labels behind
         self.x_min_entry = Entry(self, width=10)
+        self.x_min_entry.insert(0, '-5')
         self.x_max_entry = Entry(self, width=10)
+        self.x_max_entry.insert(0, '5')
         self.y_min_entry = Entry(self, width=10)
+        self.y_min_entry.insert(0, '-25')
         self.y_max_entry = Entry(self, width=10)
+        self.y_max_entry.insert(0, '25')
+
 
         self.x_min_label = Label(self, text='x min')
         self.x_max_label = Label(self, text='x max')
@@ -87,7 +89,13 @@ class Handler(Frame):
         self.y_max_entry.grid(row=3, column=1)
 
         # handling button
-        self.rescale_but = Button(self, text='rescale', command=Displayer.rescale)
+        self.rescale_but = Button(self, text='rescale', command=self.rescale(self.displayer))
         self.rescale_but.grid(row=4, column=0, columnspan=2)
 
-
+    def rescale(self, displayer):
+        print('rescaling')
+        x_min = self.x_min_entry.get()
+        x_max = self.x_max_entry.get()
+        y_min = self.y_min_entry.get()
+        y_max = self.y_max_entry.get()
+        displayer.add_axis(x_min, x_max, y_min, y_max)
--- program.py
+++ program.py
@@ -7,11 +7,11 @@ f = Function.parse(input())
 root = Tk()
 
 frame1 = Frame(root)
-handler = Handler(root)
+displayer = Displayer(frame1)
+handler = Handler(root, displayer)
 frame1.grid(row=0, column=0)
 handler.grid(row=0, column=1)
-
-displayer = Displayer(frame1)
+displayer.add_axis()
 displayer.add_function(f.calculate)
 displayer.pack()
 
