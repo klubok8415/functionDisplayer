679e95a74bdf1bf2c5318cf3b090726af0531d49
Arseny Neustroev <neustroev.arseny@gmail.com>
2018-01-26 16:40:46 +0500

    added function offset

--- gui.py
+++ gui.py
@@ -3,11 +3,9 @@ from expressions import Function
 
 
 class Displayer(Canvas):
-    def __init__(self, root,  x_min, x_max, y_min, y_max, x_scale=1, y_scale=1, canvas_size=500):
+    def __init__(self, root,  x_min, x_max, y_min, y_max, canvas_size=500):
         super(Displayer, self).__init__(root, width=canvas_size, height=canvas_size, bg="white")
         self.canvas_size = canvas_size
-        self.x_scale = x_scale
-        self.y_scale = y_scale
         self.x_min = x_min
         self.x_max = x_max
         self.y_min = y_min
@@ -28,7 +26,7 @@ class Displayer(Canvas):
                                  width=0.25, fill='black')
 
                 self.create_text(k + self.canvas_size // 2, -10 + self.canvas_size // 2,
-                                 text=str(k * self.y_scale * (self.x_max - self.x_min) // (self.x_scale * self.canvas_size)), fill='black',
+                                 text=str(k * (self.x_max - self.x_min) //  self.canvas_size), fill='black',
                                  font=('Helvectica', '10'))
         # marking y_axis
         for j in range(self.canvas_size + 1):
@@ -44,19 +42,18 @@ class Displayer(Canvas):
 
     def add_function(self, f, color="black"):
         previous_point = [0, 0]
-        for x in range(-self.canvas_size // 2, self.canvas_size // 2 + 1):
-            x = x * (self.x_max - self.x_min) / self.canvas_size
-            point = [x * self.canvas_size // (self.x_max - self.x_min) + self.canvas_size // 2,
-                     self.canvas_size // 2 - (f(x / self.x_scale) * self.y_scale) * self.canvas_size // (self.y_max - self.y_min)]
+        for x in range(self.canvas_size + 1):
+            point = [x,  self.canvas_size - (self.canvas_size / (self.y_max - self.y_min) *
+                                             (f((self.x_max - self.x_min) / self.canvas_size * x + self.x_min) - self.y_min))]
             self.create_line(previous_point, point, fill=color)
             previous_point = point
 
     def add_point(self, x, y, color="black"):
         self.create_oval(
-            self.x_scale * x - 1,
-            self.canvas_size - self.y_scale * y + 1,
-            self.x_scale * x + 1,
-            self.canvas_size - self.y_scale * y - 1,
+            x - 1,
+            self.canvas_size * y + 1,
+            x + 1,
+            self.canvas_size * y - 1,
             fill=color)
 
     def rescale(self, f, x_min, x_max, y_min, y_max):
