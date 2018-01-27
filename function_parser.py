a887c9fa69a7e63aa988f63c6ec97f14f93c3b89
girvel <widauka@ya.ru>
2018-01-27 15:44:10 +0500

    added braces parsing

--- function_parser.py
+++ function_parser.py
@@ -1,6 +1,12 @@
 from expressions import *
 
 
+class Operator:
+    def __init__(self, character, operation):
+        self.character = character
+        self.operation = operation
+
+
 class Parser:
     def __init__(self, operators):
         self.operators = operators
@@ -14,6 +20,9 @@ class Parser:
         return self._parse(string)
 
     def _parse(self, string):
+        if string[0] == "(" and string[-1] == ")":
+            string = string[1:-1]
+
         if string == "x":
             v = Value(0)
             return Function(v, [v])
@@ -26,17 +35,31 @@ class Parser:
             return Function(Value(value), [])
 
         for current_operators in self.operators:
-            operators_indices = [string.rfind(o) for o in current_operators if string.rfind(o) != -1]
-            if len(operators_indices) <= 0:
-                continue
-            operator_position = max(operators_indices)
+            operators_by_character = {o.character: o for o in current_operators}
 
-            operator_class = current_operators[string[operator_position]]
+            operator_position = len(string) - 1
+            braces = 0
+
+            for s in string[::-1]:
+                if s == "(":
+                    braces -= 1
+                elif s == ")":
+                    braces += 1
+
+                if braces == 0:
+                    operator = operators_by_character.get(s)
+                    if operator is not None:
+                        break
+
+                operator_position -= 1
+            else:
+                continue
+
             arg1 = self._parse(string[:operator_position])
             arg2 = self._parse(string[operator_position + 1:])
 
             return Function(
-                operator_class(arg1.expression, arg2.expression),
+                operator.operation(arg1.expression, arg2.expression),
                 arg1.variables + arg2.variables)
 
         raise Exception()
@@ -44,11 +67,11 @@ class Parser:
 
 default_parser = Parser([
     {
-        "+": Addition,
-        "-": Deduction,
+        Operator("+", Addition),
+        Operator("-", Deduction),
     },
     {
-        "*": Multiplication,
-        "/": Division,
+        Operator("*", Multiplication),
+        Operator("/", Division),
     },
 ])
