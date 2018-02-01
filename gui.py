b7d764d39beed0ceebbe1c55dff3304bd4482660
Arseny Neustroev <neustroev.arseny@gmail.com>
2018-02-01 16:06:51 +0500

    gui update: correct axis marking (in progress)

--- gui.py
+++ gui.py
@@ -4,46 +4,70 @@ import math
 
 class Displayer(Canvas):
 
-    def __init__(self, root, x_min=0, x_max=0, y_min=0, y_max=0, canvas_size=500):
+    def __init__(self, root, x_min=0, x_max=0, y_min=0, y_max=0, canvas_size=500, border=20):
         self.root = root
         self.canvas_size = canvas_size
+        self.border = border
         self.x_min = x_min
         self.x_max = x_max
         self.y_min = y_min
         self.y_max = y_max
-        super(Displayer, self).__init__(root, width=canvas_size, height=canvas_size, bg='white')
+        super(Displayer, self).__init__(root, width=canvas_size+self.border // 2, height=canvas_size+self.border // 2, bg='white')
 
     def _update(self, f, color='blue'):
-        x_position = self.canvas_size // 2 + (self.y_max + self.y_min) / 2 * self.canvas_size / (self.y_max - self.y_min)
-        y_position = self.canvas_size // 2 - (self.x_max + self.x_min) / 2 * self.canvas_size / (self.x_max - self.x_min)
+        x_axis_position = self.canvas_size // 2 + (self.y_max + self.y_min) / 2 * self.canvas_size / (self.y_max - self.y_min)
+        y_axis_position = self.canvas_size // 2 - (self.x_max + self.x_min) / 2 * self.canvas_size / (self.x_max - self.x_min)
 
-        self.y_axis = self.create_line(y_position, self.canvas_size, y_position, 0,
+        self.y_axis = self.create_line(y_axis_position, self.canvas_size, y_axis_position, 0,
                                        width=1, arrow=LAST, fill="gray")
-        self.x_axis = self.create_line(0, x_position, self.canvas_size, x_position,
+        self.x_axis = self.create_line(0, x_axis_position, self.canvas_size, x_axis_position,
                                        width=1, arrow=LAST, fill="gray")
-
         # marking x_axis
-        for i in range(self.canvas_size + 1):
-            if i % (self.canvas_size / 10) == 0:
-                k = -self.canvas_size // 2 + i
-                self.create_line(k + self.canvas_size // 2, -2 + x_position,
-                                 k + self.canvas_size // 2, 2 + x_position,
-                                 width=0.25, fill='gray')
+        i = 0
+        while True:
+            k = (self.x_max - self.x_min) // (self.canvas_size // 50)
 
-                self.create_text(k + self.canvas_size // 2, -10 + x_position,
-                                 text=str(k * (self.x_max - self.x_min) // self.canvas_size), fill='black',
-                                 font=('Helvectica', '10'))
+            a = k * (i + self.x_min // k)
+
+            i += 1
+
+            x = self.canvas_size*(1 - (1 - 1 / (self.x_max - self.x_min) * (a - self.x_min)))
+
+            if x > self.canvas_size:
+                break
+
+            self.create_line(x, -2 + x_axis_position, x, 2+x_axis_position,
+                             width=0.25, fill='gray')
+
+            self.create_text(x, 15+x_axis_position,
+                             text=str(a), fill='black',
+                             font=('Helvectica', '10'))
+
         # marking y_axis
-        for j in range(self.canvas_size + 1):
-            if j % (self.canvas_size / 10) == 0:
-                k = -self.canvas_size // 2 + j
-                if k != 0:
-                    self.create_line(-2 + y_position, k + self.canvas_size // 2, 2 + y_position, k + self.canvas_size // 2,
-                                     width=0.25, fill='gray')
+        i = 0
+        while True:
+            k = (self.y_max - self.y_min) // (self.canvas_size // 50)
 
-                    self.create_text(15 + y_position, k + self.canvas_size // 2,
-                                     text=str(-k * (self.y_max - self.y_min) // self.canvas_size), fill='black',
-                                     font=('Helvectica', '10'))
+
+            a = k * (i + self.y_min // k)
+
+
+            i += 1
+
+            if a == 0:
+                continue
+
+            y = self.canvas_size * (1 - 1/(self.y_max - self.y_min) * (a - self.y_min))
+
+            if y < 0:
+                break
+
+            self.create_line(-2 + y_axis_position, y, 2 + y_axis_position, y,
+                             width=0.25, fill='gray')
+
+            self.create_text(15 + y_axis_position, y,
+                             text=str(a), fill='black',
+                             font=('Helvectica', '10'))
         previous_point = None
         for x in range(self.canvas_size + 1):
             point = [x,  self.canvas_size - (self.canvas_size / (self.y_max - self.y_min) *
@@ -118,6 +142,7 @@ class MainFrame:
         self.y_max_entry.grid(row=3, column=1)
         self.function_label.grid(row=4, column=0, columnspan=2)
         self.function_entry.grid(row=5, column=0, columnspan=2)
+        self.function_entry.insert(0, 'y=x')
 
         # handling button
         self.rescale_but = Button(self.handler_frame, text='draw')
