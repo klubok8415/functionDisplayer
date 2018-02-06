b85b691da4d39c848ade75e9c21c3e92ad8d0f57
Arseny Neustroev <neustroev.arseny@gmail.com>
2018-02-06 11:43:47 +0500

    gui: root resizing (in progress)

--- gui.py
+++ gui.py
@@ -10,22 +10,23 @@ from function_parser.default import default_parser
 
 
 class Displayer(Canvas):
-    def __init__(self, root, x_min=-25, x_max=25, y_min=-25, y_max=25, canvas_size=500, border=50):
+    def __init__(self, root, x_min=-25, x_max=25, y_min=-25, y_max=25, size_x=500, size_y=500, border=50):
         self.root = root
-        self.canvas_size = canvas_size
+        self.size_x = size_x
+        self.size_y = size_y
         self.border = border
         self.x_min = x_min
         self.x_max = x_max
         self.y_min = y_min
         self.y_max = y_max
         self.functions_list = []
-        super(Displayer, self).__init__(root, width=canvas_size + self.border, height=canvas_size + self.border,
+        super(Displayer, self).__init__(self.root, width=self.size_x + self.border, height=self.size_y + self.border,
                                         bg='white')
 
-    def _update(self, color='blue'):
-        x_axis_position = self.canvas_size // 2 + (self.y_max + self.y_min) / 2 * self.canvas_size / (
+    def __update(self, color='blue'):
+        x_axis_position = self.size_y // 2 + (self.y_max + self.y_min) / 2 * self.size_y / (
         self.y_max - self.y_min) + self.border // 2
-        y_axis_position = self.canvas_size // 2 - (self.x_max + self.x_min) / 2 * self.canvas_size / (
+        y_axis_position = self.size_x // 2 - (self.x_max + self.x_min) / 2 * self.size_x / (
         self.x_max - self.x_min) + self.border // 2
 
         if x_axis_position < self.border / 2:
@@ -42,11 +43,11 @@ class Displayer(Canvas):
                 prev_x = numpy.NaN
                 prev_y = numpy.NaN
 
-                for x in range(self.canvas_size + 1):
+                for x in range(self.size_x + 1):
                     point = (
                         x + self.border // 2,
-                        self.canvas_size
-                        - (self.canvas_size * (f((self.x_max - self.x_min) / self.canvas_size * x + self.x_min) - self.y_min)
+                        self.size_y
+                        - (self.size_y * (f((self.x_max - self.x_min) / self.size_x * x + self.x_min) - self.y_min)
                            / (self.y_max - self.y_min))
                         + self.border // 2)
 
@@ -68,11 +69,11 @@ class Displayer(Canvas):
 
                 self.create_line(pp, fill=color)
 
-        self.y_axis = self.create_line(y_axis_position, self.canvas_size + self.border // 2,
+        self.y_axis = self.create_line(y_axis_position, self.size_y + self.border // 2,
                                        y_axis_position, self.border // 2,
                                        width=1, arrow=LAST, fill="gray")
         self.x_axis = self.create_line(self.border // 2, x_axis_position,
-                                       self.canvas_size + self.border // 2, x_axis_position,
+                                       self.size_x + self.border // 2, x_axis_position,
                                        width=1, arrow=LAST, fill="gray")
         # marking x_axis
         i = 0
@@ -82,16 +83,16 @@ class Displayer(Canvas):
             while (self.x_max - self.x_min) * (10 ** n) < 10:
                 n += 1
 
-            k = (self.x_max - self.x_min) * (10 ** n) // (self.canvas_size // 50)
+            k = (self.x_max - self.x_min) * (10 ** n) // (self.size_x // 50)
 
             a = k * (i + self.x_min * (10 ** n) // k)
 
             i += 1
 
-            x = self.canvas_size * (
+            x = self.size_x * (
             1 - (1 - 1 / ((self.x_max - self.x_min) * (10 ** n)) * (a - self.x_min * (10 ** n))))
 
-            if x > self.canvas_size:
+            if x > self.size_x:
                 break
 
             self.create_line(x + self.border // 2, -2 + x_axis_position, x + self.border // 2, 2 + x_axis_position,
@@ -109,7 +110,7 @@ class Displayer(Canvas):
             while (self.y_max - self.y_min) * (10 ** n) < 10:
                 n += 1
 
-            k = (self.y_max - self.y_min) * (10 ** n) // (self.canvas_size // 50)
+            k = (self.y_max - self.y_min) * (10 ** n) // (self.size_y // 50)
 
             a = k * (i + self.y_min * (10 ** n) // k)
 
@@ -118,7 +119,7 @@ class Displayer(Canvas):
             if a == 0:
                 continue
 
-            y = self.canvas_size * (1 - 1 / ((self.y_max - self.y_min) * (10 ** n)) * (a - self.y_min * (10 ** n)))
+            y = self.size_y * (1 - 1 / ((self.y_max - self.y_min) * (10 ** n)) * (a - self.y_min * (10 ** n)))
 
             if y < 0:
                 break
@@ -134,9 +135,9 @@ class Displayer(Canvas):
     def add_point(self, x, y, color="black"):
         self.create_oval(
             x - 1,
-            self.canvas_size * y + 1,
+            self.size_y * y + 1,
             x + 1,
-            self.canvas_size * y - 1,
+            self.size_y * y - 1,
             fill=color)
 
     def rescale(self, x_min, x_max, y_min, y_max):
@@ -146,32 +147,33 @@ class Displayer(Canvas):
         self.y_max = y_max
 
         self.delete(ALL)
-        self._update()
+        self.__update()
 
     def add_function(self, func):
         self.functions_list.append(func)
         self.delete(ALL)
-        self._update()
+        self.__update()
 
     def delete_function(self, func):
         self.functions_list.pop(self.functions_list.index(func))
         self.delete(ALL)
-        self._update()
+        self.__update()
 
     def clear(self):
         self.functions_list = []
         self.delete(ALL)
-        self._update()
+        self.__update()
 
 
 class MainFrame:
     def __init__(self):
         self.root = Tk()
+        self.root.bind('<Configure>', self.root_resize)
         self.canvas_frame = Frame(self.root)
-        self.handler_frame = Frame(self.root)
+        self.handler_frame = Frame(self.root, padx=20)
         self.displayer = Displayer(self.canvas_frame)
-        self.canvas_frame.grid(row=0, column=0)
-        self.handler_frame.grid(row=0, column=1)
+        self.canvas_frame.pack(side=LEFT, fill=X, expand=1)
+        self.handler_frame.pack(side=LEFT, fill=X)
         self.displayer.pack()
 
         self.limitations_frame = Frame(self.handler_frame, pady=50)
@@ -236,6 +238,19 @@ class MainFrame:
         self.clear_but.bind('<Button-1>', self.on_click_clear)
         self.clear_but.grid(row=0, column=2)
 
+        self.root.update()
+        self.root.minsize(self.root.winfo_width(), self.root.winfo_height())
+        print(self.root.winfo_width(), self.root.winfo_height())
+
+    def root_resize(self, event):
+        pass
+
+        # self.root.update()
+        # self.displayer.destroy()
+        # self.displayer = Displayer(self.root, size_x=int(self.root.winfo_width()) - 300,
+        #                            size_y=int(self.root.winfo_height()) - 56)
+        # self.displayer.update()
+
     def rescale(self, event):
 
         try:
@@ -295,6 +310,8 @@ class MainFrame:
     def on_click_clear(self, event):
         self.functions_listbox.delete(0, 'end')
         self.displayer.clear()
+
+    # methods below create background text in function entrybox
 
     def change_entry_exit(self, event):
         if self.function_entry.get():
